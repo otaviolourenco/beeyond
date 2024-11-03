3c75ab408de9707a838d4c8e7574d66c104fb5a2
Otávio Lourenço <otavioluissv@gmail.com>
2024-11-03 19:33:32 +0000

    Initial Commit

---
+++ beeyond/beeyondApp.swift
@@ -0,0 +1,17 @@
+//
+//  beeyondApp.swift
+//  beeyond
+//
+//  Created by Otavio Lourenço on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct beeyondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
