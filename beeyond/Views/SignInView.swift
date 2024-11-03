96c618681b7a2eea8465c55d33b6a174f63dda33
Otávio Lourenço <otavioluissv@gmail.com>
2024-11-03 23:35:31 +0000

    feat: Create a login view and use FirebaseAuth to authetication

---
+++ beeyond/Views/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  beeyond
+//
+//  Created by Otavio Lourenço on 03/11/2024.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
